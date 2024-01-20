14ea9736b102bb862b6504236ad304c01ee1ab6d
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2024-01-20 23:23:26 +0900

    add topPage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import Layout from "../components/layout";
-
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Layout from '../components/layout'
+import utilStyle from '../styles/utils.module.css'
 
 export default function Home() {
-	return (
-		<Layout></Layout>
-	);
+    return (
+        <Layout>
+            <section className={utilStyle.headingMd}>
+                <p>私はスタートアップ企業でエンジニアをしています</p>
+            </section>
+            <section>
+                <h2>Next.jsの学習用blogです</h2>
+                <div>
+                    <article>
+                        <Link href="/">
+                            <img src="images/thumbnail01.jpg"></img>
+                        </Link>
+                        <Link href="/">
+                            <a>Next.jsとは？</a>
+                        </Link>
+                        <br />
+                        <small>January 16, 2024</small>
+                    </article>
+                </div>
+            </section>
+        </Layout>
+    )
 }
